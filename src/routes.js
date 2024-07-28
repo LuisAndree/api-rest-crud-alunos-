5ebb6bbb109589a5000a811c5fce5858206e602b
Luís André <andre2016999@gmail.com>
2024-07-28 14:39:44 -0300

    criacao de rotas de turmas

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,19 @@ const express = require ('express');
 const router = express.Router();
 
 const AlunoController = require('./controllers/AlunoController');
+const TurmaController = require('./controllers/TurmaController');
 
+//Rotas de Aunos
 router.get('/alunos', AlunoController.listarAlunos);
 router.get('/aluno/:id', AlunoController.exibirAluno);
 router.post('/aluno', AlunoController.adicionarAluno);
 router.put('/aluno/:id', AlunoController.alterarAluno);
 router.delete('/aluno/:id', AlunoController.excluirAluno);
+
+//Rotas de turmas
+router.get('/turmas', TurmaController.listarTurmas);
+router.get('/turma/:id', TurmaController.exibirTurma);
+router.post('/turma', TurmaController.adicionarTurma);
 
 
 module.exports = router;
